cfaa36ca7d536ac403688ea62b1727ad29a1d82a
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2020-01-23 21:27:03 +0200

    Redirect input outfiles to the $(ProjectDirectory)\Testbench directory

---
+++ MINIC2C/TestBench/test.txt.c
@@ -0,0 +1,14 @@
+float a;
+void main(int argc, char* argv[]){
+	//  ***** Local declarations *****
+	
+	//  ***** Code Body *****
+	printf("res=%f\n",a=0);
+	while ( a<10 ){
+		printf("res=%f\n",a=a+1);
+		
+	}
+	
+	
+}
+
